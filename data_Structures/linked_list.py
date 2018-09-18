98e3340521de2107f06fa483dfcd67c8ca96a921
ayadav01 <ayadav01@student.coppin.edu>
2018-09-18 16:01:01 -0400

    doubly linked list conversion

--- data_Structures/linked_list.py
+++ data_Structures/linked_list.py
@@ -2,13 +2,16 @@
 class Node(object):
 	def __init__(self, value=None):
 		self.value = value
+		#double linked list
 		self.next = None
+		self.prev = None
 
 #Linked_List class (uses Node class)
 class Linked_List(object):
 	def __init__(self):
 		#empty head object
 		self.head = Node()
+		self.size = 0
 
 	def add_node(self, arg=None):
 		if arg:
@@ -18,6 +21,28 @@ class Linked_List(object):
 			while cur.next:
 				cur = cur.next
 			cur.next = new_node
+			cur.next.prev = cur
+			self.size += 1
+
+	def search_val(self, value=None):
+	    cur = self.head
+	    while cur.next:
+	      cur = cur.next
+	      if cur.value == value:
+	        return cur
+	    return False
+
+	def print_reverse(self, value):
+	    if value:
+	      rev_elem = []
+	      start_node = self.search_val(value)
+	      if start_node:
+	        while start_node.prev:
+	          rev_elem.append(start_node.value)
+	          start_node = start_node.prev
+	        return rev_elem
+	      else:
+	        return False
 
 	def print_list(self):
 		elem = []
@@ -34,7 +59,7 @@ for i in range(2,7):
 	my_list.add_node(i)
 #prit the list
 print(my_list.print_list())
-
+print(my_list.print_reverse(4))
 
 
 
